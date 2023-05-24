039637c3a12a0aadecbdce262be1cb88170d10d8
Mayrbek <ffrench325@gmail.com>
2023-05-24 08:55:30 +0300

    update control

--- js/script.js
+++ js/script.js
@@ -1,43 +1,81 @@
-let tank = document.querySelector('.tank');
-let body = document.querySelector('body');
-w = document.body.clientWidth;
-h = document.body.clientHeight;
+var tank = document.querySelector('.tank')
 
-window.addEventListener('resize', function(){
-    w = document.body.clientWidth;
-    h = document.body.clientHeight;
-});
+var tankX = 0;
+var tankY = 0;
+var tankRotation = -90;
 
-document.addEventListener('keydown', move);
-document.addEventListener('keydown', stop);
+function handleKeyPress(event) {
+    var keyCode = event.keyCode;
 
-function stop(e) {
+    if (keyCode === 87) { // Клавиша W
+    // Движение вперед
+    tankX += Math.sin(tankRotation * Math.PI / -180) * 5;
+    tankY += Math.cos(tankRotation * Math.PI / -180) * 5;
+    } else if (keyCode === 83) { // Клавиша S
+    // Движение назад
+    tankX -= Math.sin(tankRotation * Math.PI / -180) * 5;
+    tankY -= Math.cos(tankRotation * Math.PI / -180) * 5;
+    } else if (keyCode === 65) { // Клавиша A
+    // Поворот против часовой стрелки
+    tankRotation -= 5;
+    if (tankRotation < 0) {
+        tankRotation += 360;
+    }
+    } else if (keyCode === 68) { // Клавиша D
+    // Поворот по часовой стрелке
+    tankRotation += 5;
+    if (tankRotation >= 360) {
+        tankRotation -= 360;
+    }
+    }
 
+    // Обновляем стили блока
+    tank.style.transform = `translate(${tankX}px, ${tankY}px) rotate(${tankRotation}deg)`;
 }
 
-function move(e) {
-    let key = e.key;
-    let x = tank.offsetLeft;
-    let y = tank.offsetTop;
+document.addEventListener("keydown", handleKeyPress);
 
-    if(key === 'd') {
-        tank.className = 'tank';
-        tank.classList.add('right');
-        if (x < w - 145) tank.style.left = x+5+'px';
-    }
-    if(key === 's') {
-        tank.className = 'tank';
-        tank.classList.add('down');
-        if (y < h - 150) tank.style.top = y+5+'px';
-    }
-    if(key === 'a') {
-        tank.className = 'tank';
-        tank.classList.add('left');
-        if (x > 0) tank.style.left = x-5+'px';
-    }
-    if(key === 'w') {
-        tank.className = 'tank';
-        tank.classList.add('up');
-        if (y > 0) tank.style.top = y-5+'px';
-    }
-}
+
+// let tank = document.querySelector('.tank');
+// let body = document.querySelector('body');
+// w = document.body.clientWidth;
+// h = document.body.clientHeight;
+
+// window.addEventListener('resize', function(){
+//     w = document.body.clientWidth;
+//     h = document.body.clientHeight;
+// });
+
+// document.addEventListener('keydown', move);
+// document.addEventListener('keydown', stop);
+
+// function stop(e) {
+
+// }
+
+// function move(e) {
+//     let key = e.key;
+//     let x = tank.offsetLeft;
+//     let y = tank.offsetTop;
+
+//     if(key === 'd') {
+//         tank.className = 'tank';
+//         if (x < w - 145) {
+//             tank.style.transform += "rotate(-5deg)";
+//         }
+//     }
+//     if(key === 's') {
+//         tank.className = 'tank';
+//         tank.classList.add('down');
+//         if (y < h - 150) tank.style.top = y+5+'px';
+//     }
+//     if(key === 'a') {
+//         tank.className = 'tank';
+//         tank.style.transform += "rotate(5deg)";
+//     }
+//     if(key === 'w') {
+//         tank.className = 'tank';
+//         tank.classList.add('up');
+//         if (y > 0) tank.style.top = y-5+'px';
+//     }
+// }
