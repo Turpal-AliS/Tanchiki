49d90d195e1e6e16b489d537f09ed77bb59d0193
Mayrbek <ffrench325@gmail.com>
2023-05-25 10:20:56 +0300

    add updatings

--- js/script.js
+++ js/script.js
@@ -1,81 +1,114 @@
-var tank = document.querySelector('.tank')
+var tank = document.querySelector('.tank');
+var tankSecond = document.querySelector('.tankSecond');
 
 var tankX = 0;
 var tankY = 0;
 var tankRotation = -90;
+var isMovingForward = false;
+var isMovingBackward = false;
+var speed = 3; // Скорость движения танка
+
+var tankSecondX = 0;
+var tankSecondY = 0;
+var tankSecondRotation = 90;
+var isMovingForwardSecond = false;
+var isMovingBackwardSecond = false;
 
 function handleKeyPress(event) {
-    var keyCode = event.keyCode;
+  var keyCode = event.keyCode;
 
-    if (keyCode === 87) { // Клавиша W
-    // Движение вперед
-    tankX += Math.sin(tankRotation * Math.PI / -180) * 5;
-    tankY += Math.cos(tankRotation * Math.PI / -180) * 5;
-    } else if (keyCode === 83) { // Клавиша S
-    // Движение назад
-    tankX -= Math.sin(tankRotation * Math.PI / -180) * 5;
-    tankY -= Math.cos(tankRotation * Math.PI / -180) * 5;
-    } else if (keyCode === 65) { // Клавиша A
-    // Поворот против часовой стрелки
-    tankRotation -= 5;
+  if (keyCode === 87) { // Клавиша W
+    isMovingForward = true;
+  } else if (keyCode === 83) { // Клавиша S
+    isMovingBackward = true;
+  } else if (keyCode === 65) { // Клавиша A
+    tankRotation -= 3;
     if (tankRotation < 0) {
-        tankRotation += 360;
+      tankRotation += 360;
     }
-    } else if (keyCode === 68) { // Клавиша D
-    // Поворот по часовой стрелке
-    tankRotation += 5;
+  } else if (keyCode === 68) { // Клавиша D
+    tankRotation += 3;
     if (tankRotation >= 360) {
-        tankRotation -= 360;
+      tankRotation -= 360;
     }
+  } else if (keyCode === 38) { // Стрелка вверх
+    isMovingForwardSecond = true;
+  } else if (keyCode === 40) { // Стрелка вниз
+    isMovingBackwardSecond = true;
+  } else if (keyCode === 37) { // Стрелка влево
+    tankSecondRotation -= 3;
+    if (tankSecondRotation < 0) {
+      tankSecondRotation += 360;
     }
+  } else if (keyCode === 39) { // Стрелка вправо
+    tankSecondRotation += 3;
+    if (tankSecondRotation >= 360) {
+      tankSecondRotation -= 360;
+    }
+  }
+}
 
-    // Обновляем стили блока
-    tank.style.transform = `translate(${tankX}px, ${tankY}px) rotate(${tankRotation}deg)`;
+function handleKeyRelease(event) {
+  var keyCode = event.keyCode;
+
+  if (keyCode === 87) { // Клавиша W
+    isMovingForward = false;
+  } else if (keyCode === 83) { // Клавиша S
+    isMovingBackward = false;
+  } else if (keyCode === 38) { // Стрелка вверх
+    isMovingForwardSecond = false;
+  } else if (keyCode === 40) { // Стрелка вниз
+    isMovingBackwardSecond = false;
+  }
+}
+
+function updateBlockPosition() {
+  if (isMovingForward) { // Клавиша W
+    // Движение вперед
+    tankX += speed * Math.sin(tankRotation * Math.PI / -180);
+    tankY += speed * Math.cos(tankRotation * Math.PI / -180);
+  } else if (isMovingBackward) { // Клавиша S
+    // Движение назад
+    tankX -= speed * Math.sin(tankRotation * Math.PI / -180);
+    tankY -= speed * Math.cos(tankRotation * Math.PI / -180);
+  }
+
+  if (isMovingForwardSecond) { // Стрелка вверх
+    // Движение вперед
+    tankSecondX += speed * Math.sin(tankSecondRotation * Math.PI / -180);
+    tankSecondY += speed * Math.cos(tankSecondRotation * Math.PI / -180);
+  } else if (isMovingBackwardSecond) { // Стрелка вниз
+    // Движение назад
+    tankSecondX -= speed * Math.sin(tankSecondRotation * Math.PI / -180);
+    tankSecondY -= speed * Math.cos(tankSecondRotation * Math.PI / -180);
+  }
+
+  tank.style.transform = `translate(${tankX}px, ${tankY}px) rotate(${tankRotation}deg)`;
+  tankSecond.style.transform = `translate(${tankSecondX}px, ${tankSecondY}px) rotate(${tankSecondRotation}deg)`;
+
+  checkCollision();
+  requestAnimationFrame(updateBlockPosition);
+}
+
+function checkCollision() {
+  var tankRect = tank.getBoundingClientRect();
+  var tankSecondRect = tankSecond.getBoundingClientRect();
+
+  if (
+    tankRect.left < tankSecondRect.right &&
+    tankRect.right > tankSecondRect.left &&
+    tankRect.top < tankSecondRect.bottom &&
+    tankRect.bottom > tankSecondRect.top
+  ) {
+    // Танки столкнулись - останавливаем движение
+    isMovingForward = false;
+    isMovingBackward = false;
+    isMovingForwardSecond = false;
+    isMovingBackwardSecond = false;
+  }
 }
 
 document.addEventListener("keydown", handleKeyPress);
+document.addEventListener("keyup", handleKeyRelease);
 
-
-// let tank = document.querySelector('.tank');
-// let body = document.querySelector('body');
-// w = document.body.clientWidth;
-// h = document.body.clientHeight;
-
-// window.addEventListener('resize', function(){
-//     w = document.body.clientWidth;
-//     h = document.body.clientHeight;
-// });
-
-// document.addEventListener('keydown', move);
-// document.addEventListener('keydown', stop);
-
-// function stop(e) {
-
-// }
-
-// function move(e) {
-//     let key = e.key;
-//     let x = tank.offsetLeft;
-//     let y = tank.offsetTop;
-
-//     if(key === 'd') {
-//         tank.className = 'tank';
-//         if (x < w - 145) {
-//             tank.style.transform += "rotate(-5deg)";
-//         }
-//     }
-//     if(key === 's') {
-//         tank.className = 'tank';
-//         tank.classList.add('down');
-//         if (y < h - 150) tank.style.top = y+5+'px';
-//     }
-//     if(key === 'a') {
-//         tank.className = 'tank';
-//         tank.style.transform += "rotate(5deg)";
-//     }
-//     if(key === 'w') {
-//         tank.className = 'tank';
-//         tank.classList.add('up');
-//         if (y > 0) tank.style.top = y-5+'px';
-//     }
-// }
+updateBlockPosition();
